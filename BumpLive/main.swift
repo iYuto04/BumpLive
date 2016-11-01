b850da633f0fc7ee2e9dd37888e1101040c602ae
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2016-11-01 10:43:52 +0900

    Initial Commit

---
+++ BumpLive/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BumpLive
+//
+//  Created by Yuto Mizutani on 2016/11/01.
+//  Copyright © 2016年 Yuto Mizutani. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
